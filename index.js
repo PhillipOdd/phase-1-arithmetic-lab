a95787133b02cad71e87c9eaec018867040e01bd
Phillip Lloyd <phillip.lloyd@student.moringaschool.com>
2023-06-30 06:34:39 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let counter = 0;
+//=> undefined
 
-// Write your code here
+counter += 10;
+//=> 10
+
+counter -= 2;
+//=> 8
+
+counter *= 4;
+//=> 32
+
+counter /= 2;
+//=> 16
+
+counter %= 6;
+//=> 4
+
+counter **= 3;
+//=> 64
